63bc98c0b73f9b1eddc14d1ce0a9c7063e525525
Hyane Moussassa <hyane.moussassa1@gmail.com>
2024-07-04 01:42:11 +0100

    Init commit

---
+++ src/test/java/com/esgglobal/TestJavaApplication/TestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgglobal.TestJavaApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
